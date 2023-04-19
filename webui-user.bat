02c9b35e26ab81891b3b74cb90d211205e76b4b3
Vuyiswa Nkosi <he2original@gmail.com>
2023-04-19 21:03:46 +0200

    Merging blank_subject_filewords and webui-user changes

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,8 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--theme=dark --medvram --no-half-vae --clip-models-path "D:\Archive\outer\files\stable-self\cache"
+set XDG_CACHE_HOME=D:\Archive\outer\files\stable-self\cache
+set TRANSFORMERS_CACHE=D:\Archive\outer\files\stable-self\cache
 
 call webui.bat
