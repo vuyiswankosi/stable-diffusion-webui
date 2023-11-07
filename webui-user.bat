cb3421f3c81a2200866a292146bdf6cb2e52919e
Vuyiswa Nkosi <he2original@gmail.com>
2023-11-07 15:30:18 +0200

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,7 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--theme=dark --medvram --no-half-vae --clip-models-path "D:\Archive\outer\files\stable-self\cache"
+set COMMANDLINE_ARGS=--xformers --theme=dark --medvram --no-half-vae --clip-models-path "D:\Archive\outer\files\stable-self\cache"
 set XDG_CACHE_HOME=D:\Archive\outer\files\stable-self\cache
 set TRANSFORMERS_CACHE=D:\Archive\outer\files\stable-self\cache
 
